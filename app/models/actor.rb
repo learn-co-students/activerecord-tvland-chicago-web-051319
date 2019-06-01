60d31c4c9d0d7706986b648b2371eeb78a995d6c
github username <github email address>
2019-06-01 13:59:48 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        p "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+       character = Character.all.find_by(actor_id: self.id)
+       show = Show.all.find_by(id: character.show_id)
+       p "#{character.name} - #{show.name}"
+    end
+
 end
